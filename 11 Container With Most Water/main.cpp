d7b78be6766fb93d6d3bfec6043798ddbd710894
jdnull <jiaduan@ict.ac.cn>
2015-03-31 23:15:36 +0800

    finish 11

---
+++ 11 Container With Most Water/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int> &height) {
+        int i = 0;
+        int j = height.size() - 1;
+        
+        int ret = 0;
+        while(i < j)
+        {
+            int area = (j - i) * min(height[i], height[j]);
+            ret = max(ret, area);
+            
+            if (height[i] <= height[j])
+                i++;
+            else
+                j--;
+        }
+        
+        return ret;
+    }
+};
+
+#define COUNT 3
+int main() {
+    Solution s;
+    
+    return 0;
+}
