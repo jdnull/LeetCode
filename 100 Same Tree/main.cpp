569ad7178948bfb163d543f46d00edebc1ba5aab
jdnull <jiaduan1990@163.com>
2015-06-14 20:04:40 +0800

    finish 100

---
+++ 100 Same Tree/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL && q == NULL) {
+            return true;
+        } else if (p == NULL || q == NULL) {
+            return false;
+        } else {
+            if (p->val != q->val) {
+                return false;
+            } else {
+                return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+            }
+        }
+    }
+};
+
+int main() {
+    
+    return 0;
+}
