b8fb8350876ecc52e7b5ddb56dc9ee53c4e13c50
jdnull <jiaduan1990@163.com>
2015-06-20 12:10:42 +0800

    update 122

--- 122 Best Time to Buy and Sell Stock II/main.cpp
+++ 122 Best Time to Buy and Sell Stock II/main.cpp
@@ -7,6 +7,22 @@ using namespace std;
 
 class Solution {
 public:
+    // not my code, simpler
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() <= 1) {
+            return 0;
+        }
+        
+        int sum = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i]-prices[i-1] > 0) {
+                sum += prices[i] - prices[i-1];
+            }
+        }
+        
+        return sum;
+    }
+    
     int maxProfit(vector<int>& prices) {
         if (prices.size() <= 1) {
             return 0;
