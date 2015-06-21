909109705f0c452db376b8f61d245190cdef52b9
jdnull <jiaduan1990@163.com>
2015-06-21 21:18:13 +0800

    optimize 62's space complexity

--- 62 Unique Paths/main.cpp
+++ 62 Unique Paths/main.cpp
@@ -7,30 +7,53 @@ using namespace std;
 
 class Solution {
 public:
+    // optimize space complexity from O(n*n) to O(n);
     int uniquePaths(int m, int n) {
-        vector<vector<int> > vv;
-        for (int i = 0; i < m; i++) {
-            vector<int> v;
-            v.resize(n);
-            vv.push_back(v);
-        }
+        vector<int> v;
+        v.resize(n);
         
         for (int i = m-1; i >= 0; i--) {
             for (int j = n-1; j >= 0; j--) {
                 if (i == m-1 && j == n-1) {
-                    vv[i][j] = 1;
+                    v[j] = 1;
                 } else if (i == m-1) {
-                    vv[i][j] = vv[i][j+1];
+                    v[j] = v[j+1];
                 } else if(j == n-1) {
-                    vv[i][j] = vv[i+1][j];
+                    ;
                 } else {
-                    vv[i][j] = vv[i+1][j] + vv[i][j+1];
+                    v[j] = v[j] + v[j+1];
                 }
             }
         }
         
-        return vv[0][0];
+        return v[0];
     }
+    
+    // space somplexity O(n*n)
+    // int uniquePaths(int m, int n) {
+    //     vector<vector<int> > vv;
+    //     for (int i = 0; i < m; i++) {
+    //         vector<int> v;
+    //         v.resize(n);
+    //         vv.push_back(v);
+    //     }
+        
+    //     for (int i = m-1; i >= 0; i--) {
+    //         for (int j = n-1; j >= 0; j--) {
+    //             if (i == m-1 && j == n-1) {
+    //                 vv[i][j] = 1;
+    //             } else if (i == m-1) {
+    //                 vv[i][j] = vv[i][j+1];
+    //             } else if(j == n-1) {
+    //                 vv[i][j] = vv[i+1][j];
+    //             } else {
+    //                 vv[i][j] = vv[i+1][j] + vv[i][j+1];
+    //             }
+    //         }
+    //     }
+        
+    //     return vv[0][0];
+    // }
 };
 
 int main() {
