4b166eae949b8bc7fac5e55c60292176935f3fb6
jdnull <jiaduan1990@163.com>
2015-06-29 01:55:41 +0800

    update 33

--- 33 Search in Rotated Sorted Array/main.cpp
+++ 33 Search in Rotated Sorted Array/main.cpp
@@ -8,6 +8,31 @@ using namespace std;
 class Solution {
 public:
     // not my code
+    // int search(int[] A, int target) {
+    //     int lo = 0;
+    //     int hi = A.length - 1;
+    //     while (lo < hi) {
+    //         int mid = (lo + hi) / 2;
+    //         if (A[mid] == target) return mid;
+    
+    //         if (A[lo] <= A[mid]) {
+    //             if (target >= A[lo] && target < A[mid]) {
+    //                 hi = mid - 1;
+    //             } else {
+    //                 lo = mid + 1;
+    //             }
+    //         } else {
+    //             if (target > A[mid] && target <= A[hi]) {
+    //                 lo = mid + 1;
+    //             } else {
+    //                 hi = mid - 1;
+    //             }
+    //         }
+    //     }
+    //     return A[lo] == target ? lo : -1;
+    // }
+
+    // not my code
     // int search(int A[], int n, int target) {
     //     int lo=0,hi=n-1;
     //     // find the index of the smallest value using binary search.
