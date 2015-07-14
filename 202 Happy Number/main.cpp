11cfb6744248ce38c25f4b4ac838bd6a60b211f9
jdnull <jiaduan1990@163.com>
2015-07-14 20:06:12 +0800

    update 202

--- 202 Happy Number/main.cpp
+++ 202 Happy Number/main.cpp
@@ -14,6 +14,29 @@ using namespace std;
 
 class Solution {
 public:
+    // not my code, O(1) space complexity
+    // int digitSquareSum(int n) {
+    //     int sum = 0, tmp;
+    //     while (n) {
+    //         tmp = n % 10;
+    //         sum += tmp * tmp;
+    //         n /= 10;
+    //     }
+    //     return sum;
+    // }
+    
+    // bool isHappy(int n) {
+    //     int slow, fast;
+    //     slow = fast = n;
+    //     do {
+    //         slow = digitSquareSum(slow);
+    //         fast = digitSquareSum(fast);
+    //         fast = digitSquareSum(fast);
+    //     } while(slow != fast);
+    //     if (slow == 1) return 1;
+    //     else return 0;
+    // }
+
     bool isHappy(int n) {
         map<int, int> m;
         int sum = n;
