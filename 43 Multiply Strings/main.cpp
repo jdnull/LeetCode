f5da7c64c9ffcdab17f59f4a7e790c78207fb53e
jdnull <jiaduan1990@163.com>
2015-07-31 09:47:36 +0800

    finish 43, not my code

---
+++ 43 Multiply Strings/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        string sum(num1.size() + num2.size(), '0');
+        
+        for (int i = num1.size() - 1; 0 <= i; --i) {
+            int carry = 0;
+            for (int j = num2.size() - 1; 0 <= j; --j) {
+                int tmp = (sum[i + j + 1] - '0') + (num1[i] - '0') * (num2[j] - '0') + carry;
+                sum[i + j + 1] = tmp % 10 + '0';
+                carry = tmp / 10;
+            }
+            sum[i] += carry;
+        }
+        
+        size_t startpos = sum.find_first_not_of("0");
+        if (string::npos != startpos) {
+            return sum.substr(startpos);
+        }
+        return "0";
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.multiply("123", "321");
+}
---
+++ 43 Multiply Strings/readme.md
@@ -0,0 +1,18 @@
+# 43 Multiply Strings
+
+Given two numbers represented as strings, return multiplication of the numbers as a string.
+
+Note: The numbers can be arbitrarily large and are non-negative.
+
+##### Tags:
+
+Math, String
+
+##### SP:
+
+[(Meduim) Add Two Numbers](https://leetcode.com/problems/add-two-numbers/)
+
+[(Easy) Plus One](https://leetcode.com/problems/plus-one/)
+
+[(Easy) Add Binary](https://leetcode.com/problems/add-binary/)
+
